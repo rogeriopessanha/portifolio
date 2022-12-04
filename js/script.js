1e5fd495ab0a98acc749c0aeef9be69778c09956
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-12-04 01:54:17 -0300

    att

--- js/script.js
+++ js/script.js
@@ -64,35 +64,76 @@ window.onclick = function(e){
 
 
 
+  
+
+
+
+
+
+var swiper = new Swiper(".modal-principal-slider", {
+
+    loop:true,
+      spaceBetween: 20,
+      autoplay: {
+        delay: 6500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },   
+      breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          991: {
+            slidesPerView: 3,
+          },
+          1440: {
+              slidesPerView: 4,
+            },
+        },
 
 
 
 
 
 
-// var swiper = new Swiper(".projetos-slider", {
-//     loop:true,
-//     spaceBetween: 20,
-//     autoplay: {
-//       delay: 6500,
-//       disableOnInteraction: false,
-//   },
-// //   
-//     breakpoints: {
-//         0: {
-//           slidesPerView: 1,
-//         },
-//         768: {
-//           slidesPerView: 2,
-//         },
-//         991: {
-//           slidesPerView: 3,
-//         },
-//         1440: {
-//             slidesPerView: 4,
-//           },
-//       },
-//   });
+
+
+
+
+
+//    
+  });
+
+//   loop:true,
+  //     spaceBetween: 20,
+  //     autoplay: {
+  //       delay: 6500,
+  //       disableOnInteraction: false,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  // },   
+  //     breakpoints: {
+  //         0: {
+  //           slidesPerView: 1,
+  //         },
+  //         768: {
+  //           slidesPerView: 2,
+  //         },
+  //         991: {
+  //           slidesPerView: 3,
+  //         },
+  //         1440: {
+  //             slidesPerView: 4,
+  //           },
+  //       },
 
 
 
