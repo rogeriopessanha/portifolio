7e5a3d13e97108e183e3ac94ef68ca3e7dc4d62b
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-12-09 22:40:48 -0300

    att

--- js/script.js
+++ js/script.js
@@ -98,49 +98,6 @@ var swiper = new Swiper(".habilidades-slider", {
 
 
 
-// var swiper = new Swiper(".habilidades-slider", {
-//     effect: "cube",
-//     grabCursor: true,
-//     cubeEffect: {
-//       shadow: true,
-//       slideShadows: true,
-//       shadowOffset: 20,
-//       shadowScale: 0.94,
-//     },
-//     autoplay: {
-//                 delay: 4000,
-//                 disableOnInteraction: false,
-//             },
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-//   });
-
-
-
-// var swiper = new Swiper(".habilidades-slider", {
-//     effect: "coverflow",
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: "auto",
-//     coverflowEffect: {
-//         rotate: 50,
-//         stretch: 0,
-//         depth: 1000,
-//         modifier: 1,
-//         slideShadows: true,
-//     },
-//     autoplay: {
-//         delay: 4000,
-//         disableOnInteraction: false,
-//     },
-//     pagination: {
-//         el: ".swiper-pagination",
-//     },
-// });
-
-
-
 
 
 
