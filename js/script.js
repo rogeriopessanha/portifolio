cf4e3803859ab049261d2ad32fc2cb0d47c04e28
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-12-19 20:47:50 -0300

    att

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ function loader(){
     document.querySelector('.loader-container').classList.add('active');
 }
 function fadeOut(){
-    setTimeout(loader, 2000);
+    setTimeout(loader, 6000);
 }
 
 
